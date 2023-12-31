8e10a15689138156df267ead2479c576927586cd
itsnouralawad <itsnouralawad@gmail.com>
2023-12-31 12:57:24 +0000

    spes : Import and build BootCtrl v1.2

--- device.mk
+++ device.mk
@@ -13,6 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# BootCtrl
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
